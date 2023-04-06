6b62ad0bed79d01236f8b80920c8ef2ae9de8e18
Bano Qabil <teacher@banoqabil.com>
2023-04-06 14:52:22 +0500

    FEAT:JS Introduction - 1 to 9 Chap

---
+++ scripts/app.js
@@ -0,0 +1,32 @@
+// DAY 1 - 6 April 
+
+// alert('Hello World')
+// var username =prompt("Your Name PLease","Usama")
+// var user = "Usama"
+// var fuser = "Usman"
+// console.log("Hello " + user+" " +fuser)
+
+// var sawera rasheed
+// saweraRasheed camelCase
+// SaweraRasheed PascalCase
+// sawera_rasheed snake_case
+
+// console.log(2+5)
+// console.log(2-5)
+// console.log(2*5)
+// console.log(2/5)
+// console.log(2**5)
+
+// var exp = (8 + (7 / 9) - (10 / 7) * (4 - 6)) + 4
+// console.log(exp)
+
+// var age =+ prompt("Your age PLease")
+// var fage =+ prompt("Your father age PLease")
+// alert(age+fage)
+// alert(Number(age) + Number(fage))
+// console.log(typeof age)
+
+// Day 2 - 7 Apr 
+
+
+
